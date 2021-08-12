f6c4208ae3c06fd8f205934cdcbe473ebfd60cad
Jimmy Vu <JMV3211@gmail.com>
2021-08-12 15:53:57 -0700

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,17 @@
+// create your loops here.
+//prints out 0 to 9
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//increments by 2 and prints out
+var temp = 0;
+for (i = 0; i < 10; i++) {
+  console.log(temp);
+  temp += 2;
+}
+
+//100 Times in Reverse and prints out
+for (i = 100; i > 0; i--) {
+  console.log("Time till explosion:" + i + "!");
+}
